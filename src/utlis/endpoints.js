6699e80b54b5ff0f31ee3c2e5998dbdfe8e9dab3
Kiisi <kiisifelix06@gmail.com>
2023-04-14 16:37:34 +0100

    changing endpoints

--- src/utlis/endpoints.js
+++ src/utlis/endpoints.js
@@ -1,3 +1,3 @@
 // export const endpoint = 'http://localhost:5000'
-// export const endpoint = 'https://zealous-puce-attire.cyclic.app'
-export const endpoint = 'https://book-a-doc-backend.vercel.app'
+export const endpoint = 'https://zealous-puce-attire.cyclic.app'
+// export const endpoint = 'https://book-a-doc-backend.vercel.app'
